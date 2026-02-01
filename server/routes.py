5afd26197a77d37d3918f7283735299fd77a59de
Michael Frank <mffranks34@gmail.com>
2026-02-01 00:42:23 +0000

    ahhhh i'm on fire send help

--- server/routes.py
+++ server/routes.py
@@ -1,14 +1,36 @@
 # routes.py
 
 from server.coordination import events
-
+from server.coordination import tasks
+from server import db
+from coordination import scheduler
 
 def handle_heartbeat(data):
     client_id = data["id"]
 
-    # Let coordination layer process the heartbeat
-    actions = events.handle_heartbeat(client_id, data)
+    # Let coordination layer update turtle state
+    events.handle_heartbeat(client_id, data)
 
-    return {
-        "actions": actions
-    }
+    response = []
+
+    # Assign job if idle
+    job = db.next_pending_job()
+    if job:
+        assigned = scheduler.choose_turtle_for_job(job)
+        if assigned == client_id:
+            db.start_job(job["id"], client_id)
+            scheduler.start_job(client_id, job)
+            response.append({
+                "type": "job",
+                "job": job
+            })
+
+    # Send next task
+    task = tasks.next_task(client_id)
+    if task:
+        response.append({
+            "type": "task",
+            "task": task
+        })
+
+    return response
